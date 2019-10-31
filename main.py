3bf181e2656c0aaccd1567d4614be00fa3895c51
beppe95 <37592014+beppe95@users.noreply.github.com>
2019-10-31 17:51:02 +0100

    Need to reformat preprocessing pipeline!

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from pathlib import Path
 
 import pandas as pd
 
-from utils import split_in_sentences, pre_processing, get_word_embeddings
+from utils import split_in_sentences, pre_processing, get_word_embeddings, ask_embedding_dim, \
+    make_sentences_vectors, make_similarity_matrix, apply_pagerank, ask_top_n_sentences_to_extract, extract_sentences
 
 pd.set_option('display.max_columns', None)
 pd.set_option('display.expand_frame_repr', False)
@@ -15,5 +16,19 @@ if __name__ == '__main__':
     dataset.dropna(axis=0, inplace=True)
 
     sentences_list = split_in_sentences(dataset['Text'])
+
     pre_processed_sentences = pre_processing(sentences_list)
-    embeddings = get_word_embeddings('100')
+
+    embedding_dimensionality = ask_embedding_dim()
+    embeddings = get_word_embeddings(embedding_dimensionality)
+
+    sents_vects = make_sentences_vectors(pre_processed_sentences, embeddings, embedding_dimensionality)
+
+    similarity_matrix = make_similarity_matrix(sentences_list, sents_vects)
+
+    pagerank_scores = apply_pagerank(similarity_matrix)
+
+    number_sentences_to_extract = ask_top_n_sentences_to_extract()
+
+    for ex_sent in extract_sentences(number_sentences_to_extract, sentences_list, pagerank_scores):
+        print(ex_sent, "\n")
--- utils.py
+++ utils.py
@@ -1,12 +1,17 @@
 from collections import defaultdict
 from pathlib import Path
+from warnings import warn
 
+import networkx as nx
 import numpy as np
 import pandas as pd
 from nltk.corpus import stopwords
 from nltk.tokenize import sent_tokenize
+from sklearn.metrics.pairwise import cosine_similarity
 
 stop_words = stopwords.words('english')
+
+possible_inputs = ['50', '100', '200', '300']
 
 
 def split_in_sentences(dataset_text: pd.Series) -> list:
@@ -19,24 +24,6 @@ def split_in_sentences(dataset_text: pd.Series) -> list:
     for s in dataset_text:
         sentences.append(sent_tokenize(s))
     return [text for sentence in sentences for text in sentence]
-
-
-# noinspection PyTypeChecker
-def get_word_embeddings(input_dim: str) -> defaultdict:
-    """
-
-    :param input_dim:
-    :return:
-    """
-    with open(Path.cwd() / "glove" / ("glove.6B." + input_dim + "d.txt"), encoding='utf-8') as embedding_file:
-        word_embeddings = defaultdict()
-        for line in embedding_file:
-            line_values = line.split()
-            word = line_values[0]
-            coefficients = np.asarray(line_values[1:], dtype='float32')
-            word_embeddings[word] = coefficients
-
-        return word_embeddings
 
 
 def remove_stopwords(sentences: list) -> str:
@@ -58,3 +45,105 @@ def pre_processing(sentences: list) -> list:
     processed_sentences = pd.Series(sentences).replace("[^a-zA-Z]", " ")
     processed_sentences = [sentence.lower() for sentence in processed_sentences]
     return [remove_stopwords(p_sent.split()) for p_sent in processed_sentences]
+
+
+# noinspection PyTypeChecker
+def get_word_embeddings(input_dim: int) -> defaultdict:
+    """
+
+    :param input_dim:
+    :return:
+    """
+    with open(Path.cwd() / "glove" / ("glove.6B." + input_dim + "d.txt"), encoding='utf-8') as embedding_file:
+        word_embeddings = defaultdict()
+        for line in embedding_file:
+            line_values = line.split()
+            word = line_values[0]
+            coefficients = np.asarray(line_values[1:], dtype='float32')
+            word_embeddings[word] = coefficients
+
+        return word_embeddings
+
+
+def ask_embedding_dim():
+    """
+
+    :return:
+    """
+    choice = input('Choose an embedding dimensionality.\nPossible values are: 50, 100, 200, 300.')
+    if choice not in possible_inputs:
+        warn('Read carefully, your choice is not allowed!')
+        return ask_embedding_dim()
+    return choice
+
+
+def make_sentences_vectors(pre_processed_sentences: list, embeddings: defaultdict, embedding_dim: str) -> list:
+    """
+
+    :param pre_processed_sentences:
+    :param embeddings:
+    :param embedding_dim:
+    :return:
+    """
+    sentences_vectors = []
+    for sent in pre_processed_sentences:
+        if len(sent) > 0:
+            v = sum([embeddings.get(word, np.zeros(int(embedding_dim), )) for word in sent.split()]) / (
+                    len(sent.split()) + 0.001)
+        else:
+            v = np.zeros(int(embedding_dim), )
+        sentences_vectors.append(v)
+    return sentences_vectors
+
+
+def make_similarity_matrix(sentences: list, sents_vects: list) -> np.zeros:
+    """
+
+    :param sentences:
+    :param sents_vects:
+    :return:
+    """
+    sent_len = len(sentences)
+
+    similarity_matrix = np.zeros(shape=(sent_len, sent_len))
+
+    for i in range(sent_len):
+        for j in range(sent_len):
+            if i != j:
+                similarity_matrix[i, j] = cosine_similarity(sents_vects[i].reshape(1, 100),
+                                                            sents_vects[j].reshape(1, 100))
+    return similarity_matrix
+
+
+def apply_pagerank(sim_mat: np.zeros) -> dict:
+    """
+
+    :param sim_mat:
+    :return:
+    """
+    nx_graph = nx.from_numpy_array(sim_mat)
+    return nx.pagerank(nx_graph)
+
+
+def ask_top_n_sentences_to_extract() -> int:
+    """
+
+    :return:
+    """
+    choice = int(input('Choose the number of sentences to extract for summary generation'))
+    if choice < 1:
+        warn('You must extract at least one sentence!')
+        return ask_top_n_sentences_to_extract()
+    return choice
+
+
+def extract_sentences(n: int, sentences: list, scores: dict) -> list:
+    """
+
+    :param n:
+    :param sentences:
+    :param scores:
+    :return:
+    """
+    ranked_sentences = sorted(((scores[i], sent) for i, sent in enumerate(sentences)), reverse=True)
+    return ranked_sentences[:n]
